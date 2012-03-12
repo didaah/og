b95583125348143908eef5e6aa95287916353d9e
East <yd2004@gmail.com>
2012-03-12 17:37:46 +0000

    首次导入

---
+++ tpl/node.tpl.php
@@ -0,0 +1,86 @@
+<?php
+// $Id$
+?>
+
+<h1 class="og_title"><?php echo $node->title?></h1>
+<div id="og_node_content_<?php echo $node->nid?>">
+  <div class="og_node_view_node">
+    <div class="og_node_view_content">
+
+      <ul class="og_node_view_header">
+        <li class="og_node_view_header_name"><strong>来自：</strong><?php echo theme('username', $node)?></li>
+        <li class="og_node_view_header_time"><strong>发布：</strong><?php echo format_date($node->created)?></li>
+        <?php
+
+        if (!empty($node->fields)) {
+          foreach ($node->field_view as $field_type => $datas) { 
+            foreach ($datas as $type => $data) {
+              echo '<li class="og_node_view_fields_list og_node_view_fields_list_' . $field_type . ' og_node_view_fields_list_' . $type . '">';
+              switch ($field_type) {
+                case 'category': case 'tags':
+                  echo dd_get_strong($data['#name']) . str_replace('category/', $node->og->path . '/term/', $data['#content']);
+                break;
+                default: // 其它字段类型
+                  echo dd_get_strong($data['#name']) . '<span>' .  $data['#content'] . '</span>';
+                break;
+              }
+              echo '</li>';
+            }
+          }
+        }
+
+        ?>
+      </ul>
+
+      <div class="og_node_view_body clearfix"><?php echo $node->body?></div>
+    </div>
+    
+    <div class="og_node_view_links">
+
+      <?php if ($node->comment_count) {?>共 <strong><?php echo $node->comment_count?></strong> 条回应
+      <?php } else {?> 还没有回应<?php }?>
+      
+      <?php if ($node->comment_count && $node->filter_comment) : ?>
+      <a href="<?php echo $node->filter_comment?>" rel="nofollow">只看<?php echo $node->name?></a>
+      <?php endif?>
+      
+      <?php if ($node->is_update) : ?>
+      <a href="<?php echo $node->update_url?>">编辑</a>
+      <?php endif?>
+      
+      <?php if ($node->is_recycle) : ?>
+      <a href="<?php echo $node->recycle_url?>" class="confirm">放入回收站</a>
+      <?php endif?>
+      
+      <?php if ($node->is_close) : ?>
+      <a href="<?php echo $node->close_url?>" class="confirm">关闭回复</a>
+      <?php endif?>
+      
+      <?php if ($node->is_open) : ?>
+      <a href="<?php echo $node->open_url?>" class="confirm">打开回复</a>
+      <?php endif?>
+      
+      <?php if ($node->is_delete) : ?>
+      <a href="<?php echo $node->delete_url?>" class="confirm">删除</a>
+      <?php endif?>
+      
+      <a href="<?php echo $node->feed_comment_url?>" title="订阅 <?php echo $node->title;?> 的最新评论">订阅本话题</a>
+      <a href="<?php echo $node->feed_og_url?>" title="订阅 <?php echo $node->og->og_name;?> 的最新话题">订阅本小组</a>
+
+    </div>
+    
+  </div>
+  
+  <div id="og_node_comment_wrapper" class="og_node_view_comments">
+    <?php if ($node->comment_view):?>
+      <?php echo $node->comment_view?>
+      <?php echo $node->comment_pager?>
+    <?php endif?>
+    
+    <?php if ($node->is_comment) { ?>
+      <?php echo $node->comment_form?>
+    <?php } else {?>
+      <div class="og_node_view_comments_message"><?php echo $node->comment_form?></div>
+    <?php }?>
+  </div>
+</div>
---
+++ tpl/og.tpl.php
@@ -0,0 +1,80 @@
+<?php
+// $Id$
+?>
+
+<div class="og_info">
+  <div class="og_info_logo">
+    <img src="<?php echo $og->logo?>" alt="<?php echo $og->og_name ?> LOGO" title="<?php echo $og->og_name ?>"/>
+  </div>
+  
+  <div class="og_info_content">
+    <div class="og_info_header">
+      <span class="og_info_header_create">创建于<?php echo format_date($og->created); ?></span>
+      <span class="og_info_header_user">组长：<?php echo theme('username', $og); ?></span>
+    </div>
+    <div class="og_info_des"><?php echo $og->data['des']?></div>
+  </div>
+  
+  <div class="og_info_link">
+    <span class="og_info_link_node_type">小组状态：<?php echo $og->type_message ?></span>
+    
+    <span class="og_info_link_user_count">共<strong>
+    <a href="<?php echo $og->url?>/user"><?php echo $og->user_count ?></a>
+    </strong>名成员</span>
+    
+    <?php if ($og->node_count): ?>
+      <span class="og_info_link_node_count">，<strong><?php echo $og->node_count ?></strong>个话题</span>
+    <?php endif?>
+    
+    <?php if ($og->links['addtopic']) { ?>
+      <a href="<?php echo $og->links['addtopic']?>" class="og_links_add_topic">我要发言</a>
+    <?php } else {?>
+      <a href="<?php echo $og->url?>" title="请先加入小组" class="og_links_add_topic confirm_msg">我要发言</a>
+    
+      <?php if ($og->links['join']) {?>
+        <a href="<?php echo $og->links['join']?>" class="og_links_join">加入小组</a>
+      <?php } else {?>
+        <a href="<?php echo $og->url?>" class="og_links_join login_msg">加入小组</a>
+      <?php }?>
+      
+    <?php }?>
+    
+    <?php if ($og->links['logout']): ?>
+      <a href="<?php echo $og->links['logout']?>" class="og_links_logout confirm">退出小组</a>
+    <?php endif?>
+    
+    <?php if ($og->links['admin']): ?>
+      <a href="<?php echo $og->links['admin']?>" class="og_links_admin">管理小组</a>
+    <?php endif?>
+  </div>
+
+</div>
+
+<?php 
+  if (!empty($og->default_field)) {
+    $tabs = array(l(t('og', '全部话题'), $og->path));
+    foreach ($og->default_field as $term) {
+      $tabs[] = l($term->name, $og->path . '/term/' . $term->tid);
+    }
+    echo theme('item_list', $tabs, NULL, 'ul', array('class' => 'tabs'));
+  }
+?>
+
+<?php if (!empty($og->term) && $og->term->description) : ?>
+
+<div class="og_term_description">
+  <?php echo filter_view($og->term->description); ?>
+</div>
+
+<?php endif; ?>
+
+<div class="og_node">
+  <?php 
+    if (!empty($og->nodes)) {
+      echo $og->nodes;
+    } else {
+      echo system_no_content();
+    }
+    echo $og->pager;
+  ?>
+</div>
